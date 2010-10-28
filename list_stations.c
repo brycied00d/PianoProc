832472823d3efd0e38f2e7f7a410496209682fa5
Bryce Chidester <bryce@cobryce.com>
2010-10-28 02:26:12 -0700

    Just a point-in-time commit. Ignore it. Still working out how to "use" libpiano

--- list_stations.c
+++ list_stations.c
@@ -1,7 +1,13 @@
-#define _POSIX_C_SOURCE 1 /* fileno() */
-#define _BSD_SOURCE /* strdup() */
+/*
+	This is just a cheezy, stripped-down version
+	of pianobar's main.c.
+	I promise, I'll be rewriting it :)
+	
+	P.S. Why couldn't libpiano be simple like
+	pandora_connect(user, password);
+*/
 
-/* system includes */
+// XXX keepin most of the same includes for now
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
@@ -9,58 +15,77 @@
 #include <sys/select.h>
 #include <time.h>
 #include <ctype.h>
-/* open () */
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-/* tcset/getattr () */
-#include <termios.h>
-#include <pthread.h>
-#include <assert.h>
 
-/* pandora.com library */
+// LibPiano woo
 #include <piano.h>
 
-//#include "list_stations.h"
-//#include "terminal.h"
-#include "config.h"
-//#include "ui.h"
-//#include "ui_act.h"
-//#include "ui_readline.h"
-
-int main (int argc, char **argv) {
+int main ()
+{
 	PianoHandle_t ph;
-	PianoInit (&ph);
+	PianoRequestDataLogin_t reqData;
+
+	PianoInit(&ph);
+
+	// Setup the PianoRequest
+	reqData.user
+	reqData.password
+	reqData.step=0;
+	printf("Logging in... ");
+	
+	// Complete the... something?
+	do
+	{
+		reqData.data = data;
+
+		*pRet = PianoRequest (ph, &req, type);
+		if (*pRet != PIANO_RET_OK) {
+			BarUiMsg (MSG_NONE, "Error: %s\n", PianoErrorToStr (*pRet));
+			PianoDestroyRequest (&req);
+			return 0;
+		}
+
+		*wRet = BarPianoHttpRequest (waith, &req);
+		if (*wRet != WAITRESS_RET_OK) {
+			BarUiMsg (MSG_NONE, "Network error: %s\n", WaitressErrorToStr (*wRet));
+			PianoDestroyRequest (&req);
+			if (req.responseData != NULL) {
+				free (req.responseData);
+			}
+			return 0;
+		}
+
+		*pRet = PianoResponse (ph, &req);
+		if (*pRet != PIANO_RET_CONTINUE_REQUEST) {
+			if (*pRet != PIANO_RET_OK) {
+				BarUiMsg (MSG_NONE, "Error: %s\n", PianoErrorToStr (*pRet));
+				PianoDestroyRequest (&req);
+				if (req.responseData != NULL) {
+					free (req.responseData);
+				}
+				return 0;
+			} else {
+				BarUiMsg (MSG_NONE, "Ok.\n");
+			}
+		}
+		/* we can destroy the request at this point, even when this call needs
+		 * more than one http request. persistend data (step counter, e.g.) is
+		 * stored in req.data */
+		if (req.responseData != NULL) {
+			free (req.responseData);
+		}
+		PianoDestroyRequest (&req);
+	} while (*pRet == PIANO_RET_CONTINUE_REQUEST);
+
+	return 1;
+}
 
 /*
-	WaitressInit (&app.waith);
-	strncpy (app.waith.host, PIANO_RPC_HOST, sizeof (app.waith.host)-1);
-	strncpy (app.waith.port, PIANO_RPC_PORT, sizeof (app.waith.port)-1);
+PianoReturn_t PianoRequest (PianoHandle_t *, PianoRequest_t *,
+		PianoRequestType_t);
 
-	BarSettingsInit (&app.settings);
-	BarSettingsRead (&app.settings);
-
-	printf("Welcome to " PACKAGE " (" VERSION ")! "
-			"Press %c for a list of commands.\n",
-			app.settings.keys[BAR_KS_HELP]);
-
-	if (app.settings.username == NULL) {
-		printf("No username set.\n");
-		exit(1);
-	}
-	if (app.settings.password == NULL) {
-		printf("No password set.\n");
-		return 1;
-	}
-
-	{
-		PianoReturn_t pRet;
-		WaitressReturn_t wRet;
-		PianoRequestDataLogin_t reqData;
-		reqData.user = app.settings.username;
-		reqData.password = app.settings.password;
-		reqData.step = 0;
-		printf("Logging in... ");
 		if (!BarUiPianoCall (&app.ph, PIANO_REQUEST_LOGIN, &app.waith,
 				&reqData, &pRet, &wRet)) {
 			printf("ERROR!\n");
