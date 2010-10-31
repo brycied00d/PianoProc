0c34b3a67259cc30b650216304cdba690c7b07bf
Bryce Chidester <bryce@cobryce.com>
2010-10-30 17:43:55 -0700

    More work trying to get this to work with libpiano... and more work trying to figure out how to use the blasted libpiano...

--- list_stations.c
+++ list_stations.c
@@ -26,27 +26,51 @@ int main ()
 {
 	PianoHandle_t ph;
 	PianoRequestDataLogin_t reqData;
+	PianoReturn_t pRet;
+	PianoRequest_t req;
+	WaitressReturn_t wRet;
+	WaitressHandle_t waith;
+	memset (&req, 0, sizeof (req));
+	char nameBuf[100] = "bryce@cobryce.com";
+	char passBuf[100] = "cobryce";
 
 	PianoInit(&ph);
 
-/*
 	// Setup the PianoRequest
-	reqData.user=0;
-	reqData.password=0;
+	reqData.user=strdup(nameBuf);
+	reqData.password=strdup(passBuf);
 	reqData.step=0;
 	printf("Logging in... ");
+	
+	req.data = &reqData;
+	pRet=PianoRequest(&ph, &req, PIANO_REQUEST_LOGIN);
+	if (pRet != PIANO_RET_OK)
+	{
+		printf("Error: %s\n", PianoErrorToStr(pRet));
+		PianoDestroyRequest(&req);
+		return 1;
+	}
+	
+/*
+	wRet = BarPianoHttpRequest (waith, &req);
+	if(wRet != WAITRESS_RET_OK)
+	{
+		printf("Network error: %s\n", WaitressErrorToStr(wRet));
+		PianoDestroyRequest(&req);
+		if (req.responseData != NULL)
+			free(req.responseData);
+		return 1;
+	}
+*/
 
+/*
 	// Complete the... something?
 	do
 	{
 		reqData.data = data;
 
 		*pRet = PianoRequest (ph, &req, type);
-		if (*pRet != PIANO_RET_OK) {
-			BarUiMsg (MSG_NONE, "Error: %s\n", PianoErrorToStr (*pRet));
-			PianoDestroyRequest (&req);
-			return 0;
-		}
+		if (*pRet != PIANO_RET_OK)
 
 		*wRet = BarPianoHttpRequest (waith, &req);
 		if (*wRet != WAITRESS_RET_OK) {
@@ -111,7 +135,7 @@ PianoReturn_t PianoRequest (PianoHandle_t *, PianoRequest_t *,
 	// app.curStation = BarUiSelectStation (&app.ph, "Select station: ", app.settings.sortOrder, stdin);
 	// BarUiPrintStation (app.curStation);
 
-	PianoDestroy (&ph);
+	PianoDestroy(&ph);
 
 	return 0;
 }
